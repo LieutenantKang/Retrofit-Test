275852a6e09075e8ade483be17ad6b7755d0d245
Lieutenant Kang <blizzard1107@gmail.com>
2018-10-01 13:57:04 +0900

    Retrofit Test v1.0

---
+++ app/src/main/java/com/example/user/retrofittest/GitHubRepo.java
@@ -0,0 +1,13 @@
+package com.example.user.retrofittest;
+
+public class GitHubRepo {
+    private String name;
+
+    public GitHubRepo (String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
